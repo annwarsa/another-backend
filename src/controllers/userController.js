696c13e669549cc0d39655b10cacf725ae3dba7a
syaifulanwar2228 <syaifulanwar2228@gmail.com>
2024-06-08 11:30:55 +0000

    update user also

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 const userService = require('../services/userService');
 const googleBucket = require('../utils/googleBucket');
-const upload = require('../utils/multerConfig');
+const Busboy = require('busboy');
 
 
 exports.getUser = async (req, res) => {
@@ -14,21 +14,46 @@ exports.getUser = async (req, res) => {
 
 exports.updateUser = async (req, res) => {
   try {
-    upload.single('images')(req, res, async (err) => {
-      if (err) {
-        return res.status(400).json({ error: err.message });
-      }
+    const busboy = new Busboy({ headers: req.headers });
+    let fields = {};
+    let fileData = null;
 
-      const file = req.file; 
-      if (!file) {
+    busboy.on('file', (fieldname, file, _filename, _encoding, _mimetype) => {
+      if (fieldname === 'images') {
+        const chunks = [];
+        file.on('data', (chunk) => {
+          chunks.push(chunk);
+        });
+        file.on('end', () => {
+          fileData = Buffer.concat(chunks);
+        });
+      } else {
+        file.resume();
+      }
+    });
+
+    busboy.on('field', (fieldname, value) => {
+      fields[fieldname] = value;
+    });
+
+    busboy.on('finish', async () => {
+      const { username, email } = fields;
+
+      if (!fileData) {
         return res.status(400).json({ error: 'Please provide an image' });
       }
 
-      const imageUrl = await googleBucket.uploadToGoogleBucket(file);
-      const { username, email } = req.body;
+      const imageUrl = await googleBucket.uploadToGoogleBucket(fileData);
       const user = await userService.updateUser(req.userId, username, email, imageUrl);
-      res.status(200).json(user)
+      res.status(200).json(user);
     });
+
+    busboy.on('error', (err) => {
+      console.error('Error:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
+
+    req.pipe(busboy);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
