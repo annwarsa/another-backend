8da90589e0cc564d0cfe7227e3d7f37dcbd94839
syaifulanwar2228 <syaifulanwar2228@gmail.com>
2024-06-10 16:55:52 +0000

    fix mimetype

--- src/utils/multerConfig.js
+++ src/utils/multerConfig.js
@@ -1,31 +1,32 @@
-// multerConfig.js
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const mime = require('mime-types');
 
-// Create the uploads directory if it doesn't exist
 const uploadsDir = path.join(__dirname, '..', 'uploads');
 if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir);
 }
 
-// Configure Multer storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, uploadsDir);
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}${path.extname(file.originalname)}`;
-    cb(null, `${file.fieldname}-${uniqueSuffix}`);
-  },
-  mimetype: (req, file, cb) => {
-    const mimetype = mime.lookup(file.originalname) || 'application/octet-stream';
-    cb(null, mimetype);
+    const fileExtension = path.extname(file.originalname);
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`;
+    cb(null, `${file.fieldname}-${uniqueSuffix}${fileExtension}`);
   },
 });
 
-// Create the Multer instance
-const upload = multer({ storage });
+const fileFilter = (req, file, cb) => {
+  const mimeType = mime.lookup(file.originalname);
+  if (!mimeType) {
+    return cb(new Error('Invalid file type'));
+  }
+  cb(null, true);
+};
+
+const upload = multer({ storage, fileFilter });
 
 module.exports = upload;
